c8f3904b43e17277038b1f379fa484ea791c1a43
Anne Rogers <amr@cs.uchicago.edu>
2017-02-07 22:42:24 -0600

    code cleaned up but not making sense

--- snapmap2.py
+++ snapmap2.py
@@ -10,88 +10,104 @@ from time import sleep
 def get_url(lat, lon, keyword, radius, key):
 
     url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location\
-={},{}&keyword={}&radius=300&key=AIzaSyCGt79JrG0sym4cyrs6YabCyy76zpnB828".\
-format(lat, lon, keyword, radius, key)
+={},{}&keyword={}&radius={}&key={}".format(lat, lon, keyword, radius, key)
 
     return url
 
 
-IL_filename = "IL.csv"
-IL = pd.read_csv(IL_filename)
+def get_info():
+    IL_filename = "IL-100.csv"
+    IL = pd.read_csv(IL_filename)
 
-developerKeys = ["AIzaSyCGt79JrG0sym4cyrs6YabCyy76zpnB828","AIzaSyBUDrUeEyJyUNwQl1oVJCydSFPb5fCMQvw", "AIzaSyC9dbLTJ-aU2VL0r1Zhpzlxx99TrW-tpMM"]
-key = developerKeys[2]
+    developerKeys = ["AIzaSyCGt79JrG0sym4cyrs6YabCyy76zpnB828",\
+    "AIzaSyBUDrUeEyJyUNwQl1oVJCydSFPb5fCMQvw", "AIzaSyC9dbLTJ-aU2VL0r1Zhpzlxx99TrW-tpMM",\
+    "AIzaSyBkWTxpnmygafi2mFETLRumyw0OlY_ftwM"]
+    key = developerKeys[3]
 
-ids = []
-lats = []
-lngs = []
-costs = []
-countafter3 = 0
+    ids = []
+    names = []
+    lats = []
+    lngs = []
+    costs = []
+    adds = []
 
-for i in range(len(IL[:100])):
-    sleep(1)
-    address = IL.loc[i]["Address"] 
-    keyword = address.split()[0]
-    lat = IL.loc[i]["Latitude"]
-    lon = IL.loc[i]["Longitude"]
-    url = get_url(lat, lon, keyword, 200, key)
-    print (url)
-    r = requests.get(url)
-    json = r.json()
-    #print (json)
-
-    if len(json["results"]) > 1:
-        print ("more than one")
-
-    if json["results"] == []:
-        print ("oh no!") 
+    for i in range(len(IL[:100])):
+        sleep(1)
         name = IL.loc[i]["Store_Name"]
-        keyword = name.split()[0]
-        #keyword = "+".join(name.split())
-        url = get_url(lat, lon, keyword, 300, key)
+        address = IL.loc[i]["Address"] 
+        lat = IL.loc[i]["Latitude"]
+        lon = IL.loc[i]["Longitude"]
+
+        keyword = address.split()[0]
+        url = get_url(lat, lon, keyword, 200, key)
         print (url)
-        r2 = requests.get(url)
-        json = r2.json()
+        r = requests.get(url)
+        json = r.json()
+        #print (json)
 
         if len(json["results"]) > 1:
+            sleep(1)
             print ("more than one")
 
         if json["results"] == []:
-            print ("oh no again!")
-            keyword = name.split()[1]
+            sleep(1)
+            print ("oh no!") 
+            keyword = name.split()[0]
             url = get_url(lat, lon, keyword, 300, key)
-            print(url)
-            r3 = requests.get(url)
-            json = r3.json()
+            print (url)
+            r = requests.get(url)
+            json = r.json()
+
+            if len(json["results"]) > 1:
+                sleep(1)
+                print ("more than one")
 
             if json["results"] == []:
-                print("NOOO")
-                countafter3 += 1
-                print (countafter3)
-        
+                sleep(1)
+                print ("oh no again!")
+                keyword = name.split()[1]
+                url = get_url(lat, lon, keyword, 300, key)
+                print(url)
+                r = requests.get(url)
+                json = r.json()
 
-"""
-    #only works with one result
-    ids.append(json["results"][0]["place_id"])
-    print (ids)
-    lats.append(json["results"][0]["geometry"]["location"]["lat"])
-    lngs.append(json["results"][0]["geometry"]["location"]["lng"])
+                if json["results"] == []:
+                    print("NOOO")
+                    ids.append("None")
+                    names.append("None")
+                    lats.append("None")
+                    lngs.append("None")
+                    adds.append("None")
+                    costs.append("None")
+                    continue
 
-    if "price_level" in json["results"][0]:
-        costs.append(json["results"][0]["price_level"])
-    #types.append(json["results"][0]["types"])
-
-#creates new column and fills each row 
-IL["googlelat"] = lats
-IL["googlelon"] = lons
-IL["place_id"] = ids
-IL["cost"] = costs
-#IL["type"] = types
-#IL["hours"]
+        #will be accurate if one results
+        ids.append(json["results"][0]["place_id"])
+        names.append(json["results"][0]["name"])
+        lats.append(json["results"][0]["geometry"]["location"]["lat"])
+        lngs.append(json["results"][0]["geometry"]["location"]["lng"])
+        adds.append(json["results"][0]["vicinity"])
 
 
-return IL
-"""
+        if "price_level" in json["results"][0]:
+            costs.append(json["results"][0]["price_level"])
+        else:
+            costs.append("None")
+        #types.append(json["results"][0]["types"])
+
+    #creates new column and fills each row 
+    IL["place_id"] = ids
+    IL["googlename"] = names
+    IL["googlelat"] = lats
+    IL["googlelon"] = lngs
+    IL["googleaddress"] = adds
+    IL["cost"] = costs
+
+    #IL["type"] = types
+
+    #return IL
+    IL.to_csv("snapresults.csv")
+
 
 
 """
