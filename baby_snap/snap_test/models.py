2d19e8f5c4161748d68cb90bb92f05433d767ec7
Ari Anisfeld <anisfeld@cs.uchicago.edu>
2017-02-19 16:38:45 -0600

    geom

--- baby_snap/snap_test/models.py
+++ baby_snap/snap_test/models.py
@@ -3,8 +3,7 @@ from django.db import models
 # Create your models here.
 class SnapLocations(models.Model):
     googlename = models.CharField(max_length=100)
-    lat = models.FloatField()
-    lon = models.FloatField()
+    point = models.PointField(sid = 4312)
     googleaddress = models.CharField(max_length = 200)
 
     # Returns the string representation of the model.
