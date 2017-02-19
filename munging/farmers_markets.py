a7216fbb8d445d3a0a48217388e32a8c1b36181a
emmacooperpeterson <emmacooperpeterson@gmail.com>
2017-02-19 14:59:51 -0600

    added lat/lon request

--- munging/farmers_markets.py
+++ munging/farmers_markets.py
@@ -3,12 +3,13 @@ import urllib3
 import csv
 import re
 import pandas as pd
+import requests
 
 
-def get_markets(url, filename):
+def get_markets(filename):
 
     pm = urllib3.PoolManager()
-    html = pm.urlopen(url=url, method="GET").data
+    html = pm.urlopen(url="http://www.dhs.state.il.us/page.aspx?item=44172", method="GET").data
     soup = bs4.BeautifulSoup(html, "lxml")
 
     tags = soup.find_all("ul", class_ = ["discBullets", "discbullets", "disBullets"])
@@ -40,13 +41,16 @@ def get_markets(url, filename):
             if len(re.findall("\(\d{3}\)\s\d{3}-\d{4}", market.text)) > 0:
                 phone = re.findall("\(\d{3}\)\s?\d{3}-\d{4}", market.text)[0]
 
-            market_list.append([market_name, address, city, state, zipcode, phone, double_value])
+            url = get_url(address, city, state)
+            lat, lon = get_lat_lon(url)
+
+            market_list.append([market_name, address, city, state, zipcode, phone, lat, lon, double_value])
 
 
     #number of markets should be 102
 
     final_list = []
-    header = ['market name', 'address', 'city', 'state', 'zipcode', 'phone', 'double value']
+    header = ['market name', 'address', 'city', 'state', 'zipcode', 'phone', 'latitude', 'longitude', 'double value']
 
     final_list.append(header)
 
@@ -63,104 +67,25 @@ def get_markets(url, filename):
     return f
 
 
-def get_url(name, address, city_state, key):
+def get_url(address, city, state):
 
-    c_s = city_state.replace(',', '')
-    query_lst =  name.split() + c_s.split() + address.split()
+    query_lst = address.split() + city.split() + state.split()
     query = "+".join(query_lst)
+    key = "AIzaSyDQsfHu5QVO-s3Xttn_twLLr6E3ggsIZDk"
 
-    url = "https://maps.googleapis.com/maps/api/place/textsearch/xml?query={}&key={}".format(query, key)
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}".format(query, key)
 
     return url
 
 
-'''
-def get_place_id():
-    IL_filename = "IL-100.csv"
-    IL = pd.read_csv(IL_filename)
+def get_lat_lon(url):
+    r = requests.get(url)
+    json = r.json()
 
-    developerKeys = ["AIzaSyCGt79JrG0sym4cyrs6YabCyy76zpnB828",\
-    "AIzaSyBUDrUeEyJyUNwQl1oVJCydSFPb5fCMQvw", "AIzaSyC9dbLTJ-aU2VL0r1Zhpzlxx99TrW-tpMM",\
-    "AIzaSyBkWTxpnmygafi2mFETLRumyw0OlY_ftwM"]
-    
-    key = developerKeys[3]
+    if json['results'] == []:
+        return (None, None)
 
-    ids = []
+    lat = json["results"][0]["geometry"]["location"]["lat"]
+    lon = json["results"][0]["geometry"]["location"]["lng"]
 
-    for i in range(len(IL[:100])):
-        sleep(1)
-        name = IL.loc[i]["Store_Name"]
-        address = IL.loc[i]["Address"] 
-        lat = IL.loc[i]["Latitude"]
-        lon = IL.loc[i]["Longitude"]
-
-        keyword = address.split()[0]
-        url = get_url(lat, lon, keyword, 200, key)
-        print (url)
-        r = requests.get(url)
-        json = r.json()
-
-        if len(json["results"]) > 1:
-            sleep(1)
-            print ("more than one")
-
-        if json["results"] == []:
-            sleep(1)
-            print ("oh no!") 
-            keyword = name.split()[0]
-            url = get_url(lat, lon, keyword, 300, key)
-            print (url)
-            r = requests.get(url)
-            json = r.json()
-
-            if len(json["results"]) > 1:
-                sleep(1)
-                print ("more than one")
-
-            if json["results"] == []:
-                sleep(1)
-                print ("oh no again!")
-                keyword = name.split()[1]
-                url = get_url(lat, lon, keyword, 300, key)
-                print(url)
-                r = requests.get(url)
-                json = r.json()
-
-                if json["results"] == []:
-                    print("NOOO")
-                    ids.append("None")
-                    names.append("None")
-                    lats.append("None")
-                    lngs.append("None")
-                    adds.append("None")
-                    costs.append("None")
-                    continue
-
-        #will be accurate if one results
-        ids.append(json["results"][0]["place_id"])
-        names.append(json["results"][0]["name"])
-        lats.append(json["results"][0]["geometry"]["location"]["lat"])
-        lngs.append(json["results"][0]["geometry"]["location"]["lng"])
-        adds.append(json["results"][0]["vicinity"])
-
-
-        if "price_level" in json["results"][0]:
-            costs.append(json["results"][0]["price_level"])
-        else:
-            costs.append("None")
-        #types.append(json["results"][0]["types"])
-
-    #creates new column and fills each row 
-    IL["place_id"] = ids
-    IL["googlename"] = names
-    IL["googlelat"] = lats
-    IL["googlelon"] = lngs
-    IL["googleaddress"] = adds
-    IL["cost"] = costs
-
-    #IL["type"] = types
-
-    #return IL
-    IL.to_csv("snapresults.csv")
-
-    '''
+    return (lat, lon)
