2167cf8c61419842c45460cde1e98993f724911a
Ari Anisfeld <anisfeld@cs.uchicago.edu>
2017-02-25 14:23:06 -0600

    middle of updating models

--- baby_snap_2/snap_test_2/data_to_model.py
+++ baby_snap_2/snap_test_2/data_to_model.py
@@ -13,11 +13,20 @@ csv_file = 'practice.csv'
 
 with open(csv_file) as f:
     ls = list(csv.reader(f))
+    # UPDATE THESE
+    name_index = ls[0].index('googlename')
+    lat_index = ls[0].index('googlename')
+    lon_index = ls[0].index('googlename')
+    place_id_index = ls[0].index('googlename')
+
     for line in ls[1:]:
         googlename = line[1]
         geom = Point(float(line[3]), float(line[2]))
         googleaddress = line[4]
+        place_id = line[9]
         try:
-            SnapLocations(googlename = googlename, geom = geom, googleaddress = googleaddress).save()
+            SnapLocations(googlename = googlename, geom = geom, 
+                googleaddress = googleaddress, place_id = place_id).save()
         except:
             print("Trouble in row {}".format(i))
+
