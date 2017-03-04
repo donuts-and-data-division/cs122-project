e1eb24c590e0eef6101a885ebb188dddad85be9d
joan-wang <joanyiqunwang@gmail.com>
2017-03-04 12:55:11 -0600

    merged groceries with baby snap 2

--- baby_snap_2/snap_test_2/models.py
+++ baby_snap_2/snap_test_2/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-#from django.contrib.gis.db import models as gismodels
 from django.contrib.gis.db import models as gismodels
+from django.utils import timezone
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class SnapLocations(models.Model):
@@ -12,3 +15,14 @@ class SnapLocations(models.Model):
     # Returns the string representation of the model.
     def __str__(self):              
         return self.googlename
+
+
+class FoodPrices(models.Model):
+    food_name = models.CharField(max_length=100)
+    food_quantity = models.CharField(max_length=20)
+    food_price = models.FloatField()
+    date_last_updated = models.DateTimeField(default=timezone.now)
+
+    # Returns the string representation of the model.
+    def __str__(self):              
+        return self.food_name
