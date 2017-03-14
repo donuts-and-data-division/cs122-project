482d086ddf79279757c7e7f730c56c9caa0d1231
Ari Anisfeld <anisfeld@cs.uchicago.edu>
2017-03-14 16:37:36 -0500

    auto_multipliers

--- baby_snap_2/snap_test_2/management/commands/pull_user_data.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from pull_user_data import get_user_data
-
-class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
-
-    def pull_user_data(self, *args, **options):
-        print("Hooray")
