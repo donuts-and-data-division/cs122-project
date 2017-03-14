00b5ef4225d90d2506c06b38ab5400ab456a1868
Ari Anisfeld <anisfeld@cs.uchicago.edu>
2017-03-14 13:45:08 -0500

    cleaning urls/views

--- baby_snap_2/snap_test_2/urls.py
+++ baby_snap_2/snap_test_2/urls.py
@@ -8,10 +8,7 @@ from . import views
 urlpatterns = [
     url(r'^home$', views.home),
     url(r'^ajax/get_places/$', views.get_places),
-    url(r'^ajax/go_to_prices$', views.prices),
     url(r'^groceries/(?P<store_id>\w+)/$', views.submit_grocery_list, name='grocery_list'),
-    url(r'^prices$', views.prices),
-    url(r'^groceries$', views.submit_grocery_list, name='grocery_list'),
     url(r'^ajax/cash_register/$', views.cash_register, name='cash_register'),
     url(r'^submit-prices$', views.submit_prices, name='submit_prices'),
     url(r'^submit-prices/(?P<store_id>\w+)/(?P<food_string>[\d+&?]+)/$', views.submit_prices, name='price_page'),
